69b8af803d6c9ffeaf1a5e050adc6b735603487d
Kilppux <Kilppux@hotmail.com>
2018-01-29 13:17:42 +0200

    Ensimmäinen oikea committi
    Nappula spawnaa meloneita 5/5

---
+++ Scripts/ButtonSpawner.cs
@@ -0,0 +1,28 @@
+﻿namespace VRTK{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class ButtonSpawner : VRTK_InteractableObject {
+
+		public GameObject spawner;
+		public GameObject mellon;
+
+		public override void StartUsing(VRTK_InteractUse usingObject)
+		{
+			base.StartUsing(usingObject);
+			Spawn();
+		}
+
+		void Start()
+		{
+			Time.fixedDeltaTime = 0.01f;
+		}
+
+		private void Spawn()
+		{
+			Debug.Log ("spawnaus");
+			GameObject mellonClone = Instantiate(mellon, spawner.transform.position, gameObject.transform.rotation) as GameObject;
+		}
+	}
+}
