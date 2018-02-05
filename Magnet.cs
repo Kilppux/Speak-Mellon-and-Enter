153c81c5060e27b6b2ebd565372227f126fcd175
IdaRu <tuulikki.93@hotmail.com>
2018-02-05 14:35:17 +0200

    Magneetti
    Muutoksia magneettiin. Toimii jotenkuten mutta kesken.

--- Magnet.cs
+++ Magnet.cs
@@ -28,10 +28,14 @@
             Debug.Log("Magneettia kaytetaan");
             RaycastHit _hit;
             Debug.DrawRay(transform.position, transform.forward, Color.red);
-            if (Physics.Raycast(transform.position, Vector3.forward, out _hit))
+            if (Physics.Raycast(transform.position, Vector3.forward, out _hit, 100.0f))
             {
                 Debug.Log("Magneettivoimaaaa");
-
+                if (_hit.collider.gameObject.name == "mellon")
+                {
+                    _hit.collider.gameObject.transform.position = magneettiKarki.transform.position;
+                    Debug.Log("Meloniiiii");
+                }
             }
         }
     }
