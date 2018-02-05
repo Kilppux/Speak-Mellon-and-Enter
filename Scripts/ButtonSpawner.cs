ba4611140afcf963e02a61bb4cc5f0b7009ba342
Jenna Lindroos <jennam.lindroos@gmail.com>
2018-02-05 13:17:58 +0200

    MellonSound
    Melonin ääniefekti skripti, ja Buttoniin lisätty spawn ääniefekti

--- Scripts/ButtonSpawner.cs
+++ Scripts/ButtonSpawner.cs
@@ -3,10 +3,15 @@
 	using System.Collections.Generic;
 	using UnityEngine;
 
-	public class ButtonSpawner : VRTK_InteractableObject {
+	public class ButtonSpawner : VRTK_InteractableObject 
+{
 
 		public GameObject spawner;
 		public GameObject mellon;
+
+		public AudioClip spawnSound;
+		AudioSource audioSource;
+
 
 		public override void StartUsing(VRTK_InteractUse usingObject)
 		{
@@ -17,12 +22,15 @@
 		void Start()
 		{
 			Time.fixedDeltaTime = 0.01f;
+			audioSource = GetComponent<AudioSource> ();
 		}
 
 		private void Spawn()
 		{
 			Debug.Log ("spawnaus");
 			GameObject mellonClone = Instantiate(mellon, spawner.transform.position, gameObject.transform.rotation) as GameObject;
+			audioSource.PlayOneShot (spawnSound);
 		}
+
 	}
 }
---
+++ Scripts/MellonSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MellonSound : MonoBehaviour {
+
+	public AudioClip hitSound;
+	AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter()
+	{
+		Debug.Log ("aania");
+		audioSource.PlayOneShot (hitSound);
+	}
+}
