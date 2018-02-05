4060650d013cd6d74aab95c546324a6798d2d7dd
sonicrainboom7 <fullerdisaster@gmail.com>
2018-02-05 11:55:31 +0200

    Koris, roskis ja PisteHudi

---
+++ Trashcan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashcan : MonoBehaviour {
+    public ParticleSystem splatter;
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        Destroy(col.gameObject);
+        splatter.Play();
+
+    }
+}
