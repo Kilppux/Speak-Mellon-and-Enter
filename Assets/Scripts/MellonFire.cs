d55cc9f52685a9a8a5cd22aba5186d6ff9d4f7e0
sonicrainboom7 <fullerdisaster@gmail.com>
2018-02-09 09:23:40 +0200

    Add files via upload

---
+++ Assets/Scripts/MellonFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MellonFire : MonoBehaviour {
+    public Transform cannonTip;
+    public GameObject mellon;
+    
+    
+    public float speed;
+    Vector3 randomDir;
+    float timer = 1.0f;
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <=0)
+        {
+            float randomX = Random.Range(-1.4f, -0.6f);
+            randomDir = new Vector3(randomX, 2, -1);
+            Debug.Log(randomDir);
+
+            GameObject mellonFab;
+            mellonFab = Instantiate(mellon, cannonTip.position, transform.rotation);
+            Rigidbody mellonRb = mellonFab.GetComponent<Rigidbody>();
+            mellonRb.AddForce(randomDir * speed, ForceMode.Impulse);
+            
+            timer = 1.0f;
+
+        }
+         
+        
+
+    }
+}
---
+++ Assets/Scripts/MellonNinja.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MellonNinja : MonoBehaviour {
+    GameObject mellon;
+    GameObject mellonscript;
+
+	// Use this for initialization
+	void Start () {
+        mellon = GameObject.Find("mellon");
+        mellonscript = mellon.GetComponent<Squish>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OntriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "mellon")
+        {
+            col.Squished();
+        }
+        
+    }
+}
